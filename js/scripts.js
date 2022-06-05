00ce7ae9d3050678bf1d68a7eca2ce98c78ca607
aya misrabi <ayamisrabi@ayas-MacBook-Pro.local>
2022-06-05 13:16:17 -0700

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -187,20 +187,20 @@ $(document).ready(function () {
             title: "Nader and Aya's Wedding",
 
             // Event start date
-            start: new Date('Sept 24, 2020 09:00'),
+            start: new Date('Jun 30, 2022 08:30'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Sept 25 , 2020 00:00'),
+            end: new Date('Jun 30 , 2022 11:59'),
 
             // Event Address
-            address: 'Mars',
+            address: 'Ebla Hotel',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact 3amak at xxx."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Misrabi Family."
         }
     });
 
